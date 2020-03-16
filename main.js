b76d12e226f8fca4c937a8054808bcca199af33e
jeSuisCodeuse <creativegirl@outlook.fr>
2020-03-16 07:08:15 +0100

    playing around to improve it

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// when click on the car it changes color, color written beneath the car, and sound! then svg's head moves
+// when click on the car it changes color, color written beneath the car, and sound! then svg's head moves   
+let car = document.querySelector('.fas fa-car-side');
+
+car.addEventListener('click',changeColor);
+
+function changeColor(){
+    this.style.color = 'yellow'; 
+}
+
