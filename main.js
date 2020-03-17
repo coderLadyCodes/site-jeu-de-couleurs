7fda86ac7ce109b21a8fda0da697b7903b4a1465
jeSuisCodeuse <creativegirl@outlook.fr>
2020-03-17 14:39:26 +0100

    css and js not completed yet

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // when click on the car it changes color, color written beneath the car, and sound! then svg's head moves   
 let car = document.querySelector('a');
+let colors = ["red", "blue", "green", "yellow", "black", "white", "pink", "purple", "orange", "grey"];
+for (let i = 0; i < colors.length; i++);
+
 
 car.addEventListener('click',changeColor);
 
 function changeColor(){
-    this.style.color = 'yellow'; 
+    this.style.color = 'blue'; 
 }
 
