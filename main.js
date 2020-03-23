82e2b4f310a7aa78bd47c10e6c3464b8c7cc91e0
jeSuisCodeuse <creativegirl@outlook.fr>
2020-03-23 11:40:36 +0100

    some more javascript

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-// when click on the car it changes color, color written beneath the car, and sound! then svg's head moves   
-
 // to click on the car image
 const car = document.querySelector('a');
 
-//colors array
-let colors = ['red', 'blue', 'green', 'yellow', 'black', 'white', 'pink', 'purple', 'orange', 'grey'];
 
-// add event listener+ function
+let colors = ['red', 'blue', 'green', 'yellow', 'black', 'white', 'pink', 'purple', 'orange', 'grey', 'brown', 'turquoise'];
+
+//let sound = ['audio/rouge.mp3', 'audio/bleu.mp3', 'audio/vert.mp3', 'audio/jaune.mp3', 'audio/noir.mp3', 'audio/blanc.mp3', 'audio/rose.mp3', 'audio/violet.mp3', 'audio/orange.mp3', 'audio/gris.mp3', 'audio/marron.mp3', 'audio/turquoise.mp3'];
+
+let colorSound = [new Audio('red.mp3'), new Audio('blue.mp3'), new Audio('green.mp3'), new Audio('yellow.mp3'), new Audio('black.mp3'), new Audio('white.mp3'), new Audio('pink.mp3'), new Audio('purple.mp3'), new Audio('orange.mp3'), new Audio('grey.mp3'), new Audio('brown.mp3'), new Audio('turquoise.mp3')];
+
+let i = 0 ;
 car.addEventListener('click', function(){
-    
-    let randomColor = colors[Math.floor(Math.random() * colors.length)];
-     car.style.color = randomColor;
-     
-})
+     i++;
+     car.style.color = colors[i];
+     colorSound[i].play();
+     if(i === colors.length - 1){
+          i = -1; 
+          
+     }
 
-
+     })
+   
+ 
+  
+  
