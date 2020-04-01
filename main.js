0b44c59c540c54f8cc19e2820097e26a9b5729e5
jeSuisCodeuse <creativegirl@outlook.fr>
2020-04-01 13:01:52 +0200

    some css

--- main.js
+++ main.js
@@ -5,25 +5,23 @@ let colors = ['red', 'blue', 'green', 'yellow', 'black', 'white', 'pink', 'purpl
 
 let colorSound = [new Audio('red.mp3'), new Audio('blue.mp3'), new Audio('green.mp3'), new Audio('yellow.mp3'), new Audio('black.mp3'), new Audio('white.mp3'), new Audio('pink.mp3'), new Audio('purple.mp3'), new Audio('orange.mp3'), new Audio('grey.mp3'), new Audio('brown.mp3'), new Audio('turquoise.mp3')];
 
+
 let i = 0 ;
 
 car.addEventListener('click', function(){
      i++;
      
      car.style.color = colors[i];
-     colorSound[i].play(); 
+     colorSound[i].play();   
+     
 
      if(i === colors.length - 1){
           i = -1;  
+    
      }
 
-     });
-   
-     /*
-          const couleurs = document.getElementById('mots').childNodes;
-          couleurs[i].text;
-     */     
-   
+     })
+     
  
     /* 
      function myFunction(){
